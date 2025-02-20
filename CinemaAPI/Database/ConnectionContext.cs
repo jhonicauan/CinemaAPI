390d3c304b5409e2dd5dbe1efb5d3d8bb32e0280
jhoni cauan <jhonielias8@gmail.com>
2025-02-20 00:02:31 -0300

    Conexao com banco configurada

---
+++ CinemaAPI/Database/ConnectionContext.cs
@@ -0,0 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace CinemaAPI.Database;
+
+public class ConnectionContext : DbContext
+{
+    
+}
---
+++ CinemaAPI/Program.cs
@@ -0,0 +1,21 @@
+using CinemaAPI.Database;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddDbContext<ConnectionContext>(o => o.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+   app.UseSwagger();
+   app.UseSwaggerUI(s => s.SwaggerEndpoint("/swagger/v1/swagger.json", "Cinema v1"));
+}
+
+app.Run();
+
